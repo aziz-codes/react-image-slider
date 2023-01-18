43079c9dea05837e7d918a76fdea0aa16fce8230
aziz-codes <azizisoffline@gmail.com>
2023-01-19 02:27:52 +0500

    Final commit-> complete responsive image slider with dots and arrows using tailwindcss and unsplash for images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { image } from './assets/images';
+import {ChevronRightIcon, ChevronLeftIcon} from '@heroicons/react/24/outline';
 const App = () => {
+   const [currentIndex, setcurrentIndex] = useState(0);
   return (
-    <div className='text-red-500'>App</div>
+    <div className='group w-full h-screen max-w-[1400px] relative m-auto py-16 px-14 '>
+      <div className=' w-full h-full rounded-2xl relative bg-center bg-cover duration-500 '
+      style={{backgroundImage: `url(${image[currentIndex].url})`}}></div>
+      <div className='hidden group-hover:block absolute top-[50%]  -translate-x-0 translate-y-[-50%] bg-transparent  m-1' onClick={()=>{
+        currentIndex===0 ? setcurrentIndex(0): setcurrentIndex((prevState)=>prevState-1)
+      }}>
+        <ChevronLeftIcon className='h-7 w-7   rounded-full hover: animate-pulse cursor-pointer text-white'/>
+      </div>
+      <div  className='hidden group-hover:block absolute top-[50%]  translate-x-0 translate-y-[-50%] bg-transparent  m-1 right-14' onClick={()=>{
+        currentIndex===image.length-1 ? setcurrentIndex(0): setcurrentIndex((prevState)=>prevState+1)
+      }}>
+        <ChevronRightIcon className='h-7 w-7   rounded-full hover: animate-pulse cursor-pointer text-white'/>
+      </div>
+      <div className='flex top-4 justify-center py-2 gap-2'>
+      {image.map((_,index)=>(
+        <div className={`w-2 h-2 cursor-pointer rounded-full ${currentIndex===index ? 'bg-sky-500' : 'bg-slate-500'} `} onClick={()=>{setcurrentIndex(index)}}></div>
+      ))}
+      </div>
+    </div>
   )
 }
 
---
+++ src/assets/images.js
@@ -0,0 +1,19 @@
+export const image = [
+    {
+      url: "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGFwdG9wc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+      url: "https://images.unsplash.com/photo-1544099858-75feeb57f01b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGxhcHRvcHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+
+    },
+  {
+    url: "https://images.unsplash.com/photo-1580522154071-c6ca47a859ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGxhcHRvcHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+  },
+{
+  url: "https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+},
+{
+  url: "https://images.unsplash.com/photo-1558864559-ed673ba3610b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGxhcHRvcHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+}
+  ];
+  
